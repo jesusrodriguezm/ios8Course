913f8ff0fc13aa03b8dc1318f1bb3cfa1a3cb9f4
Jesús Rodríguez <jesusrodriguezm@gmail.com>
2014-10-12 15:25:08 -0600

    practicing with variables

---
+++ 02_Variables/Variables.playground/section-1.swift
@@ -0,0 +1,20 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var firstInteger:Int = 0, secondInteger:Int = 29
+var thirdInteger:Float = -1000858
+var x:Int = 5
+
+//i will by using this later to track the discounts at this department store - it will change seasonly
+var discoutAtDepartmentStore:Double = 0.3
+
+// first line
+// second line
+//UITableViewDataSource
+
+/*
+paragraph1 : Explanation
+
+paragrapg2 : Explanation
+*/
