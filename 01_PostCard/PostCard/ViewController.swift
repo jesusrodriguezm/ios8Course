0415fe91850525305e09fba5af4fb9f882447e8b
Jesús Rodríguez <jesusrodriguezm@gmail.com>
2014-10-12 09:28:10 -0600

    nameLabel functionallity (lesson challenge)

--- 01_PostCard/PostCard/ViewController.swift
+++ 01_PostCard/PostCard/ViewController.swift
@@ -15,7 +15,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var enterNameTextField: UITextField!
     @IBOutlet weak var enterMessageTextField: UITextField!
     @IBOutlet weak var mailButtom: UIButton!
-    
+    @IBOutlet weak var nameLabel: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -31,10 +32,35 @@ class ViewController: UIViewController {
         messageLabel.text = enterMessageTextField.text
         messageLabel.textColor = UIColor.redColor()
         
+        nameLabel.hidden = false;
+        nameLabel.text = enterNameTextField.text
+        nameLabel.textColor = UIColor.blueColor()
+        
         enterMessageTextField.text = ""
+        enterNameTextField.text = ""
         enterMessageTextField.resignFirstResponder()
         
+        
+        
         mailButtom.setTitle("Mail sent", forState:UIControlState.Normal)
+        
+    }
+    
+    
+    @IBAction func restartButtonPress(sender: AnyObject) {
+        mailButtom.setTitle("Send Mail", forState:UIControlState.Normal)
+        
+        messageLabel.text = ""
+        messageLabel.hidden = true
+       
+        nameLabel.text = ""
+        nameLabel.hidden = true
+        nameLabel.resignFirstResponder()
+
+        enterNameTextField.text = ""
+        enterMessageTextField.text = ""
+        enterMessageTextField.resignFirstResponder()
+
         
     }
 
